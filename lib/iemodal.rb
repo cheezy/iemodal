6c78d00228cf891003acfb7ac243c1fd7e3e118f
cheezy <cheezy@leandog.com>
2011-09-20 09:12:04 -0400

    updated module to remove duplication

--- lib/iemodal.rb
+++ lib/iemodal.rb
@@ -1,9 +1,10 @@
 require "iemodal/version"
+require 'pp'
 
 module IEModal
 
 	def self.included(cls)
-		alias_method "page_object_modal_dialog" "modal_dialog"
+		alias_method "page_object_modal_dialog", "modal_dialog"
 		define_method("modal_dialog") do |*args|
 			return iemodal_watir_modal_dialog(*args) if is_ie_watir_webdriver
 			return iemodal_selenium_modal_dialog(*args) if is_ie_selenium_webdriver
@@ -12,27 +13,26 @@ module IEModal
 	end		
 		
 	def iemodal_watir_modial_dialog(&block)
-		original_handles = browser.wd.window_handles
-		yield if block_given?
-		handles = nil
-		browser.wait do
-  			handles = browser.wd.window_handles
-  			handles.size == original_handles.size + 1
-		end
-		modal = (handles - original_handles).first
-		browser.wd.switch_to.window modal
+		handle_modal_dialog(browser.wd, &block)
 	end
 	
 	def iemodal_selenium_modal_dialog(&block)
-		original_handles = browser.window_handles
+		handle_modal_dialog(driver, &block)
+	end
+	
+	private
+	
+	def handle_modal_dialog(driver, &block)
+		original_handles = driver.window_handles
 		yield if block_given?
 		handles = nil
-		browser.wait.until do
-  			handles = browser.window_handles
+		driver.wait.until do
+  			handles = driver.window_handles
   			handles.size == original_handles.size + 1
 		end
 		modal = (handles - original_handles).first
-		browser.switch_to.window modal
+		pp :handles => handles, :original_handles => original_handles, :modal => modal
+		driver.switch_to.window modal
 	end
 	
 	def is_ie_watir_webdriver
